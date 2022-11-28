0176bf37fcd204880a1c1cbdde58c4f289d5af64
diego londoño <diegoferlondo13@gmail.com>
2022-11-27 20:43:08 -0500

    uso de la directiva v-text con interpolacion en html y creando un objeto en data

--- vue.js
+++ vue.js
@@ -1,5 +1,6 @@
 new Vue({
     el:"#vm",
     data: {
-        imagen: 'https://cnnespanol.cnn.com/wp-content/uploads/2022/07/220713165438-rba-web-nasa-full-169.jpg?quality=100&strip=info&w=384&h=216&crop=1'
+        imagen: 'https://cnnespanol.cnn.com/wp-content/uploads/2022/07/220713165438-rba-web-nasa-full-169.jpg?quality=100&strip=info&w=384&h=216&crop=1',
+        texto:"Bienvenidos al Curso Vue.Js 2"
     }})
